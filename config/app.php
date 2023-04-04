897d94dff472df5f27cfa8c3c1eeb7f61f18f2cb
vikarmaulanaarrisyad <vikar.maulana.arrisyad@gmail.com>
2023-04-04 20:06:16 +0700

    add plugin yajra datatables

--- config/app.php
+++ config/app.php
@@ -185,7 +185,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Yajra\DataTables\DataTablesServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -212,6 +212,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'DataTables' => Yajra\DataTables\Facades\DataTables::class,
     ])->toArray(),
 
 ];
