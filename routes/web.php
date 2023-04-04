a10fb97ce1cbc7e18641da9827656d59b1ddc5bf
vikarmaulanaarrisyad <vikar.maulana.arrisyad@gmail.com>
2023-04-04 19:18:36 +0700

    add role middleware and dashboard controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,12 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::group([
+    'middleware' => ['auth', 'role:admin,user']
+], function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::group([
+        'middleware' => 'role:admin'
+    ], function () {
+        //
+    });
 });
