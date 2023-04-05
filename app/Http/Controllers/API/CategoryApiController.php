2958acfa87022012dc33183f3ddd892a6e7bdbfc
armaelis <armaelis9@gmail.com>
2023-04-06 01:59:12 +0700

    add image storage api category

--- app/Http/Controllers/API/CategoryApiController.php
+++ app/Http/Controllers/API/CategoryApiController.php
@@ -5,12 +5,17 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class CategoryApiController extends Controller
 {
     public function index()
     {
         $data = Category::all();
+        foreach ($data as $key => $value) {
+            # code...
+            $value->image = Storage::url($value->image);
+        }
 
         return response()->json(['result' => $data]);
     }
