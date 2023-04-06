fb19ad789ba33dc91ba60b85686a654ad8f09b28
vikarmaulanaarrisyad <vikar.maulana.arrisyad@gmail.com>
2023-04-06 23:18:17 +0700

    update minor placeApiCOntroller storage

--- app/Http/Controllers/API/PlaceApiController.php
+++ app/Http/Controllers/API/PlaceApiController.php
@@ -5,13 +5,17 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Place;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class PlaceApiController extends Controller
 {
     public function getPlaceByCategory(Request $request, $id)
     {
         $place = Place::where('category_id', $id)->get();
-
+        foreach ($place as $key => $value) {
+            # code...
+            $value->image = Storage::url($value->image);
+        }
         return response()->json(['result' => $place]);
     }
 }
