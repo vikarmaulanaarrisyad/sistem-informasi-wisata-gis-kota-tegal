354cc7ce38c4a2d4515019ba5411f0a3291b4bf5
vikarmaulanaarrisyad <vikar.maulana.arrisyad@gmail.com>
2023-04-17 21:39:05 +0700

    refactory sidebar

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,8 +1,6 @@
 <aside class="main-sidebar sidebar-light-purple elevation-4">
     <!-- Brand Logo -->
     <a href="{{ url('/') }}" class="brand-link bg-purple">
-        <img src="{{ asset('AdminLTE/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
-            class="brand-image img-circle elevation-3 bg-light" style="opacity: .8">
         <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
     </a>
 
