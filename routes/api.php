0268fdcfb5b559b3bb55119118d78ff2b4ca6001
vikarmaulanaarrisyad <vikar.maulana.arrisyad@gmail.com>
2023-04-21 07:04:28 +0700

    update api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\API\CategoryApiController;
 use App\Http\Controllers\API\PlaceApiController;
-use App\Http\Controllers\Api\SettingApiController;
+use App\Http\Controllers\API\SettingApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,4 +24,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/categories', [CategoryApiController::class, 'index']);
 Route::get('/place', [PlaceApiController::class, 'index']);
 Route::get('/place/category/{id}', [PlaceApiController::class, 'getPlaceByCategory']);
-Route::get('setting', [SettingApiController::class, 'index']);
+Route::get('/setting', [SettingApiController::class, 'index']);
